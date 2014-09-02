8bcf61f1451514b4f7c13c6600ef1cc63b7fa4c0
xin <skywater@gmail.com>
2014-09-02 14:50:53 +0800

    Update list_use_lambda_sort.py

--- list_use_lambda_sort.py
+++ list_use_lambda_sort.py
@@ -5,12 +5,7 @@ lst = [['lucy', '25', 'beijing'], ['lilei', '32', 'hongkong'],
        ['hanmeimei', '9', 'sichuan'], ['tom', '13', 'shanghai']]
 
 
-def age(i):
-    return int(i[1])
 
-for i in lst:
-    print age(i)
-
-lst.sort(key=age)
+lst.sort(key=i : int(i[1])
 
 print lst

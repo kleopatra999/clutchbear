92e422d32c38150a700aeb7488e9c8ad37d18d85
xin <skywater@gmail.com>
2015-07-02 14:25:48 +0800

    Update number_english.py
    将阿拉伯数字转换成英文数字读法

--- number_english.py
+++ number_english.py
@@ -4,8 +4,9 @@
 # @Author  : Your Name (you@example.org)
 # @Link    : http://example.org
 # @Version : $Id$
-one_nine = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
-            'nine']
+
+zero_nine = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+             'eight', 'nine']
 ten_nineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
                 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 twenty_ninety = ['twenty', 'thirty', 'forty', 'fifty',
@@ -17,16 +18,16 @@ number = int(raw_input('input a number:').strip())
 print number, 'is',
 
 if 100 < number < 1000:
-    print one_nine[number / 100 - 1], 'hundred and',
+    print zero_nine[number / 100], 'hundred and',
 number = number - (number / 100) * 100
-# print number
+
 if 20 <= number <= 99:
     print twenty_ninety[number / 10 - 2] + ' -',
 
 number = number - (number / 10) * 10
-# print number
+
 if 9 < number < 20:
     print ten_nineteen[number - 10],
 
 if number <= 9:
-    print one_nine[number - 1],
+    print zero_nine[number],
